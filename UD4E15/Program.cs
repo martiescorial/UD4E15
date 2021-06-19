47ac191c607462d430d3759430037c1bf238f2f3
Martina <martinaescorial9@gmail.com>
2021-06-19 15:12:09 +0200

    Add project files.

---
+++ UD4E15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UD4E15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 8;
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero " + numero + " es divisible entre 2");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + numero + " no es divisible entre 2");
+            }
+        }
+    }
+}
---
+++ UD4E15/ProgramBase.cs
@@ -0,0 +1,13 @@
+﻿namespace UD4E15
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            int numero = 8;
+            if (numero % 2 == 0)
+                System.Console.WriteLine(numero + "es divisible entre 2")
+         } else
+        {
+    }
+} }
